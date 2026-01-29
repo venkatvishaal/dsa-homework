94b410f49223923c11053312f57a3d4ea8995587
Venkatvishaal T S <venkatvishaalts@gmail.com>
2026-01-29 20:05:58 +0530

    level-02 was added and Largest Element was added

---
+++ level-02/Largest Element.java
@@ -0,0 +1,10 @@
+/*
+https://www.naukri.com/code360/problems/largest-element-in-the-array-largest-element-in-the-array_5026279?leftPanelTabValue=SUBMISSION
+
+Approach:
+1.Bruteforce:
+    Initalize a temporary variable and traverse the array and update the temp variable after each traversal
+2.Logic:
+    Compare each element with max value(Current)
+*/
+
