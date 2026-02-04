cd8226fa4923e6faae109a956c4de251349759b2
Venkatvishaal T S <venkatvishaalts@gmail.com>
2026-02-04 11:11:55 +0530

    commments added

--- level-03/medium/Rotaed_SortedArrsy.java
+++ level-03/medium/Rotaed_SortedArrsy.java
@@ -1,3 +1,13 @@
+/*
+https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/1907097378/
+Approach:
+Binary search
+Logic:
+Since the integer array is sorted in ascending order. The nums array is rotated left by k prior.
+we have to first find the roated array after roating left by k index and the search for the array.
+we check whether the number in left or right of the target is sorted.
+
+*/
 class Solution {
     public int search(int[] nums, int target) {
         int l = 0;
@@ -9,8 +19,6 @@ class Solution {
             if (nums[mid] == target) {
                 return mid;
             }
-
-            // Left half is sorted
             if (nums[l] <= nums[mid]) {
                 if (target >= nums[l] && target < nums[mid]) {
                     r = mid - 1;
@@ -18,7 +26,6 @@ class Solution {
                     l = mid + 1;
                 }
             }
-            // Right half is sorted
             else {
                 if (target > nums[mid] && target <= nums[r]) {
                     l = mid + 1;
