4ecea147950ec2c5ba79d3ae7ac623b41ce08483
Venkatvishaal T S <venkatvishaalts@gmail.com>
2026-02-06 22:41:27 +0530

    level-06 folder creater and inside it in Easy folder lowercase.java added

---
+++ level-06/Easy/Lowercase.java
@@ -0,0 +1,20 @@
+/*
+Approach:
+    Convert to lowercase by functions
+Logic:
+    use in built fn
+*/
+class Solution {
+    public String toLowerCase(String s) 
+    {
+      String n= s.toLowerCase();  
+      return n;
+    }
+}
+/*
+TimeComplexity:
+Best:O(n)
+Average:O(n)
+Worst:O(n)
+SpaceComplexity:O(n)
+*/
