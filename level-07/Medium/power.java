f19e4b7cb7c47ff9bbb2eb0bbeea1ab6cd7c57bc
Venkatvishaal T S <venkatvishaalts@gmail.com>
2026-02-07 23:16:31 +0530

    changes made  added

--- level-07/Medium/power.java
+++ level-07/Medium/power.java
@@ -7,6 +7,24 @@ Logic:
 reduce the power by half and at each iteration square the base and reduce the power.
 
 */
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;          
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double result = 1.0;
+        while (N > 0) {
+            if (N % 2 == 1) {
+                result *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+        return result;
+    }
+}
 
 /* 
 Time complexity:
